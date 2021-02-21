3804d8bb62d3abc7a8f7693547ada0cf0172d187
Jonathan Ludwig <jr.ludwig@gmail.com>
2021-02-21 14:11:04 -0700

    Got rudimentary event list working. #12
    Need to convert time stamps and show sensor names.

--- ESPAlarmController/data/html/EventList.js
+++ ESPAlarmController/data/html/EventList.js
@@ -2,7 +2,36 @@ app.component('event-list-view', {
     template:
     /*html*/
     `
-<div class="display-group">
+<div id="events-list">
+    <fieldset>
+        <legend>Alarm Events</legend>
+        <div v-for="event in events">
+            {{ event }}
+        </div>
+    </fieldset>
 </div>
-`
+`,
+    data() {
+        return {
+            events: []
+        }
+    },
+    methods: {
+        getEvents() {
+            axios.
+                get('/alarm_system/events').
+                then(response => this.gotEvents(response.data)).
+                catch(error => console.log('Failed to get alarm system activity log: ' + error));
+        },
+        gotEvents(events) {
+            this.events = events.split('\n');
+        }
+    },
+    mounted() {
+        this.getEvents();
+        this.polling = setInterval(this.getEvents, 11 * 1000);
+    },
+    beforeUnmount() {
+        clearInterval(this.polling);
+    }
 });
