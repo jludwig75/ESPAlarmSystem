cbd3c819def51dea726a4144e2dc350a20c63cef
Jonathan Ludwig <jr.ludwig@gmail.com>
2021-02-25 17:50:05 -0700

    Added AlarmPersistentState unittest

---
+++ ESPAlarmController/test/AlarmPersistentState_test.cpp
@@ -0,0 +1,24 @@
+#include <catch.hpp>
+
+#include "AlarmPersistentState.h"
+
+
+SCENARIO( "Test AlarmPersistentState", "" )
+{
+    AlarmPersistentState persistState;
+    REQUIRE(persistState.begin());
+    REQUIRE(persistState.get() == AlarmPersistentState::AlarmState::Disarmed);  // Default value
+
+    WHEN( "The state is set and reloaded" )
+    {
+        REQUIRE(persistState.set(AlarmPersistentState::AlarmState::Armed));
+
+        persistState = AlarmPersistentState();
+        REQUIRE(persistState.begin());
+
+        THEN( "the state is restored" )
+        {
+            REQUIRE(persistState.get() == AlarmPersistentState::AlarmState::Armed);  // Default value
+        }
+    }
+}
